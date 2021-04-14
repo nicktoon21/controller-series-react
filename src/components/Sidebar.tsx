7228a11c3ba7ac9fe4ed5bb8845ad6d0648ea3a8
Nicolas <nicolas98aguiar@gmail.com>
2021-04-14 19:55:45 -0300

    Finalização da barra lateral

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,96 +1,157 @@
+import { ReactNode } from 'react';
+
 import clsx from 'clsx';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
-import InboxIcon from '@material-ui/icons/Inbox';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import MailIcon from '@material-ui/icons/Mail';
-import ListMenu from '../components/ListMenu';
+
+import Box from '@material-ui/core/Box';
+
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
+import PlayCircleFilledWhiteOutlinedIcon from '@material-ui/icons/PlayCircleFilledWhiteOutlined';
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
 import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
 import FindInPageOutlinedIcon from '@material-ui/icons/FindInPageOutlined';
+
+import { ReactComponent as Login } from '../assets/login.svg';
+import { ReactComponent as Logout } from '../assets/logout.svg';
 
 import { makeStyles } from '@material-ui/core/styles';
 import { useState } from 'react';
 
 
 const useStyles = makeStyles({
-    list: {
-      width: 350,
-    },
-    fullList: {
-      width: 'auto',
-    },
-    fab: {
-      position: 'absolute',
-      top: "3px",
-      left: "5px",
-    },
-  });
+  list: {
+    width: 350,
+  },
+  fullList: {
+    width: '15rem',
+  },
+  fab: {
+    position: 'absolute',
+    top: "3px",
+    left: "5px",
+  },
+  box: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+});
 
+interface ListMenuProps {
+  name: string;
+  children: ReactNode;
+}
+
+function ListMenu(props: ListMenuProps) {
+  return (
+    <>
+      <List>
+        <ListItem button key={props.name}>
+
+          <ListItemIcon>
+            {props.children}
+          </ListItemIcon>
+
+          <ListItemText primary={props.name} />
+        </ListItem>
+
+      </List>
+    </>
+  );
+}
 
 export default function Sidebar() {
 
-    const classes = useStyles();
-    const [state, setState] = useState({ left:false });
+  const classes = useStyles();
+  const [state, setState] = useState({ left: false });
 
-    const toggleDrawer = (open: boolean) => (
-        event: React.KeyboardEvent | React.MouseEvent,
-      ) => {
-        if (
-          event.type === 'keydown' &&
-          ((event as React.KeyboardEvent).key === 'Tab' ||
-            (event as React.KeyboardEvent).key === 'Shift')
-        ) {
-          return;
-        }
-    
-        setState({ ...state,'left': open});
-      };
+  const toggleDrawer = (open: boolean) => (
+    event: React.KeyboardEvent | React.MouseEvent,
+  ) => {
+    if (
+      event.type === 'keydown' &&
+      ((event as React.KeyboardEvent).key === 'Tab' ||
+        (event as React.KeyboardEvent).key === 'Shift')
+    ) {
+      return;
+    }
 
-      const list= () => (<div
-        className={clsx(classes.list, {
-          [classes.fullList]:'left',
+    setState({ ...state, 'left': open });
+  };
+
+  const list = () => (<div
+    className={clsx(classes.list, {
+      [classes.fullList]: 'left',
     })}
-        role="presentation"
-        onClick={toggleDrawer( false)}
-        onKeyDown={toggleDrawer( false)}
-        >
-            <List>
-                <ListMenu name="Teste">
-                  <InboxIcon />
-                </ListMenu>
-            </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-            </List>
-        </div>
-        )
-      
-    return (
-      <div>
-        <Button  onClick={toggleDrawer(true)}>{
-          <Fab 
+    role="presentation"
+    onClick={toggleDrawer(false)}
+    onKeyDown={toggleDrawer(false)}
+  >
+    <List>
+
+      <ListMenu name="Home">
+        <HomeOutlinedIcon />
+      </ListMenu>
+
+      <ListMenu name="Profile">
+        <AccountCircleOutlinedIcon />
+      </ListMenu>
+
+      <ListMenu name="Favorite">
+        <PlayCircleFilledWhiteOutlinedIcon />
+      </ListMenu>
+
+    </List>
+    <Divider />
+    <List>
+
+      <ListMenu name="Settings">
+        <SettingsOutlinedIcon />
+      </ListMenu>
+
+      <ListMenu name="Search">
+        <FindInPageOutlinedIcon />
+      </ListMenu>
+
+    </List>
+  </div>
+  )
+
+  return (
+
+    <div>
+      <Button onClick={toggleDrawer(true)}>{
+        <Fab
           color="primary"
           aria-label="add"
           className={classes.fab}
-          >
-            <AddIcon />
-          </Fab>
-          }</Button>
-        <Drawer open={state['left']} onClose={toggleDrawer(false)}>
+        >
+          <AddIcon />
+        </Fab>}
+      </Button>
+
+      <Drawer open={state['left']} onClose={toggleDrawer(false)}>
+        <Box>
           {list()}
-        </Drawer>
+        </Box>
+        <Box className={classes.box}>
+          <Button>
+            <Login />
+            {"Login"}
+          </Button>
+          <Button>
+            <Logout />
+            {"Logout"}
+          </Button>
+        </Box>
+      </Drawer>
     </div>
-    )
+  )
 }
